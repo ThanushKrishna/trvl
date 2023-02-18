606dfe569580c5eb63258e0eb67923c7934e66eb
thanushkrishna <jeevanthanush@gmail.com>
2023-02-18 18:12:49 +0530

    cards and pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import Navbar  from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import Singup from './components/pages/SingUp';
+
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
           <Navbar />
           <Routes>
             <Route exact path="/"  element={<Home />} />
+            <Route path="/products"  element={<Products />} />
+            <Route path="/services"  element={<Services />} />
+            <Route path="/sign-up"  element={<Singup />} />            
           </Routes>
       </Router>
       </>
---
+++ src/components/pages/SingUp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../../App.css'
+
+export default function Singup() {
+  return (
+    <h1 className='sign-up'>Singup</h1>
+  )
+}
+
